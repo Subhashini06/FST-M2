9755d64b1f076c3d013703ab8fb88fdf0bee38a4
Subhashini06 <46435033+Subhashini06@users.noreply.github.com>
2022-08-14 21:29:04 +0530

    Update Activity2.java

--- junit/Activity2.java
+++ junit/Activity2.java
@@ -2,6 +2,7 @@ package activities;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class Activity2 {
